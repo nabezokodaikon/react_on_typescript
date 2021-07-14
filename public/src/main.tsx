f64da05d1416f770cfdf31d49ea42a8ad8a91966
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-07-14 20:25:57 +0900

    WIP

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,7 +1,50 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-// ReactDOM.render(
-  // <App />,
-  // document.getElementById("root")
-// );
+const ThemeContext = React.createContext('light');
+
+interface IButtonProps {
+  theme: React.ContextType<typeof ThemeContext>;
+}
+
+class Button extends React.Component<IButtonProps> {
+  constructor(props: IButtonProps) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button>Button</button>
+    );
+  }
+}
+
+class ThemedButton extends React.Component {
+  static contextType = ThemeContext;
+  render() {
+    return <Button theme={this.context}></Button>
+  }
+}
+
+function Toolbar() {
+  return (
+    <>
+      <ThemedButton />
+    </>
+  );
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <ThemeContext.Provider value="dark">
+        <Toolbar />
+      </ ThemeContext.Provider>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
