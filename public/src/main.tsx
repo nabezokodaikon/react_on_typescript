ae968482dd71a07c05e319ffeddb4791a1e0b42b
nabezokodaikon <nabezoko.daikon@gmail.com>
2021-07-18 02:02:56 +0900

    Add empty tsx

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,70 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-interface ISquareProps {
-  value: number;  
-}
 
-class Square extends React.Component<ISquareProps> {
-  render() {
-    return (
-      <button className="square" onClick={function() {alert("click");}}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i: number) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+  // <App />,
+  // document.getElementById('root')
+// );
